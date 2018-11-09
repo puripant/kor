1ba0e810ebd034051ddea6697b2b1968fb7ee7ce
Puripant <puripant@gmail.com>
2018-11-10 02:18:47 +0700

    Add button to switch words New font

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-let wordcut = require("wordcut");
+const wordcut = require("wordcut");
 wordcut.init();
-let split_words = words
+
+const split_words = words
   .map(w => wordcut.cut(w).split("|"))
   .filter(a => a.length == 2);
-console.log(split_words);
+
+function changeWord() {
+  let word = split_words[Math.floor(Math.random()*split_words.length)];
+  let verb = document.getElementById("verb");
+  let noun = document.getElementById("noun");
+  verb.innerHTML = word[1];
+  noun.innerHTML = word[0];
+}
