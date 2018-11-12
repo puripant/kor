24a9a54d46ba9ae4907131256de982012cd6c207
Puripant <puripant@gmail.com>
2018-11-12 22:40:23 +0700

    Check part of speech

--- main.js
+++ main.js
@@ -6,9 +6,17 @@ const split_words = words
   .filter(a => a.length == 2);
 
 function changeWord() {
-  let word = split_words[Math.floor(Math.random()*split_words.length)];
-  let verb = document.getElementById("verb");
-  let noun = document.getElementById("noun");
-  verb.innerHTML = word[1];
-  noun.innerHTML = word[0];
+  let found = false;
+  while(!found) {
+    let word = split_words[Math.floor(Math.random()*split_words.length)];
+    console.log(word[0] + ": " + nouns.indexOf(word[0]) + " & " + verbs.indexOf(word[0]));
+    console.log(word[1] + ": " + nouns.indexOf(word[1]) + " & " + verbs.indexOf(word[1]));
+    if (nouns.indexOf(word[0]) > -1 && verbs.indexOf(word[1]) > -1) {
+      let noun = document.getElementById("noun");
+      let verb = document.getElementById("verb");
+      noun.innerHTML = word[0];
+      verb.innerHTML = word[1];
+      found = true;
+    }
+  }
 }
